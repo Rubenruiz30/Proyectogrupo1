ac5534a26d54bb8844b77f16f3359c336dd40d88
Ruben <davidruizglobal30@gmail.com>
2025-10-29 11:28:47 +0100

    ejercicio1

---
+++ src/proyect1.java
@@ -0,0 +1,5 @@
+public class proyect1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/proyectogit.java
@@ -0,0 +1,5 @@
+public class proyectogit {
+    public static void main(String[] args) {
+        System.out.println("Versión inicial del proyecto");
+    }
+}
